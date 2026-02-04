d5f409b91a553d20c38342ab5ba9338e3de7017d
Rasel Ahmmed <rahmmed@myseneca.ca>
2026-02-03 22:06:09 -0500

    WS03 lab complete

--- Documents/lab/CC.cpp
+++ Documents/lab/CC.cpp
@@ -1,112 +1,117 @@
 #include <iostream>
-#include "CC.h"
-
 using namespace std;
+#include "CC.h"
+#include "cstr.h"
 
 namespace seneca {
 
-CC::CC() {
-    set();
-}
+   CC::CC() {
+      m_name = nullptr;
+      m_number = 0;
+      m_cvv = 0;
+      m_expMon = 0;
+      m_expYear = 0;
+   }
 
-CC::CC(const char* name,
-       unsigned long long number,
-       short cvv,
-       short expMon,
-       short expYear) {
-    set();
-    set(name, number, cvv, expMon, expYear);
-}
+   CC::CC(const char* name,
+          unsigned long long number,
+          short cvv,
+          short expMon,
+          short expYear) {
+      m_name = nullptr;
+      set(name, number, cvv, expMon, expYear);
+   }
 
-CC::~CC() {
-    deallocate();
-}
+   CC::~CC() {
+      dealloc();
+   }
 
-void CC::deallocate() {
-    delete[] m_name;
-    m_name = nullptr;
-}
+   void CC::dealloc() {
+      delete[] m_name;
+      m_name = nullptr;
+   }
 
-void CC::aloCopy(const char* name) {
-    deallocate();
-    m_name = new char[strlen(name) + 1];
-    strcpy(m_name, name);
-}
+   void CC::aloCopy(const char* name) {
+      if (name) {
+         m_name = new char[strlen(name) + 1];
+         strcpy(m_name, name);
+      }
+   }
 
-void CC::set() {
-    deallocate();
-    m_number = 0;
-    m_cvv = 0;
-    m_expMonth = 0;
-    m_expYear = 0;
-}
+   void CC::set() {
+      dealloc();
+      m_number = 0;
+      m_cvv = 0;
+      m_expMon = 0;
+      m_expYear = 0;
+   }
 
-bool CC::isEmpty() const {
-    return m_name == nullptr;
-}
+   bool CC::validate(const char* name,
+                     unsigned long long cardNo,
+                     short cvv,
+                     short expMon,
+                     short expYear) const {
+      return name && strlen(name) > 2 &&
+             cardNo >= 4000000000000000ull &&
+             cardNo <= 4099999999999999ull &&
+             cvv >= 100 && cvv <= 999 &&
+             expMon >= 1 && expMon <= 12 &&
+             expYear >= 24 && expYear <= 32;
+   }
 
-bool CC::validate(const char* name,
-                  unsigned long long number,
-                  short cvv,
-                  short expMon,
-                  short expYear) const {
-    return name && strlen(name) > 2 &&
-           number >= 4000000000000000ULL &&
-           number <= 4099999999999999ULL &&
-           cvv >= 100 && cvv <= 999 &&
-           expMon >= 1 && expMon <= 12 &&
-           expYear >= 24 && expYear <= 32;
-}
+   void CC::set(const char* name,
+                unsigned long long number,
+                short cvv,
+                short expMon,
+                short expYear) {
+      set();
+      if (validate(name, number, cvv, expMon, expYear)) {
+         aloCopy(name);
+         m_number = number;
+         m_cvv = cvv;
+         m_expMon = expMon;
+         m_expYear = expYear;
+      }
+   }
 
-void CC::set(const char* name,
-             unsigned long long number,
-             short cvv,
-             short expMon,
-             short expYear) {
-    set();
-    if (validate(name, number, cvv, expMon, expYear)) {
-        aloCopy(name);
-        m_number = number;
-        m_cvv = cvv;
-        m_expMonth = expMon;
-        m_expYear = expYear;
-    }
-}
+   bool CC::isEmpty() const {
+      return m_name == nullptr;
+   }
 
-void CC::display(const char* name,
-                 unsigned long long number,
-                 short expYear,
-                 short expMon,
-                 short cvv) const {
-    char lname[31]{};
-    strncpy(lname, name, 30);
-    cout << "| ";
-    cout.width(30);
-    cout.setf(ios::left);
-    cout << lname << " | ";
-    prnNumber(number);
-    cout << " | " << cvv << " | ";
-    cout.unsetf(ios::left);
-    cout.width(2);
-    cout << expMon << "/" << expYear << " |" << endl;
-}
+   void CC::prnNumber(unsigned long long CCnum) const {
+      cout << CCnum / 1000000000000ull << " ";
+      CCnum %= 1000000000000ull;
+      cout << CCnum / 100000000ull << " ";
+      CCnum %= 100000000ull;
+      cout << CCnum / 10000ull << " ";
+      cout << CCnum % 10000ull;
+   }
 
-void CC::display() const {
-    if (isEmpty()) {
-        cout << "Invalid Credit Card Record" << endl;
-    } else {
-        display(m_name, m_number, m_expYear, m_expMonth, m_cvv);
-    }
-}
+   void CC::display(const char* name,
+                    unsigned long long number,
+                    short expYear,
+                    short expMon,
+                    short cvv) const {
+      char lname[31]{};
+      strcpy(lname, name, 30);
+      cout << "| ";
+      cout.width(30);
+      cout.setf(ios::left);
+      cout << lname << " | ";
+      prnNumber(number);
+      cout << " | " << cvv << " | ";
+      cout.unsetf(ios::left);
+      cout.width(2);
+      cout << expMon << "/" << expYear << " |" << endl;
+   }
 
-void CC::prnNumber(unsigned long long ccnum) const {
-    cout << ccnum / 1000000000000ULL << " ";
-    ccnum %= 1000000000000ULL;
-    cout << ccnum / 100000000ULL << " ";
-    ccnum %= 100000000ULL;
-    cout << ccnum / 10000ULL << " ";
-    cout << ccnum % 10000ULL;
-}
+   void CC::display() const {
+      if (isEmpty()) {
+         cout << "Invalid Credit Card Record" << endl;
+      } else {
+         display(m_name, m_number, m_expYear, m_expMon, m_cvv);
+      }
+   }
 
 }
 
--- Documents/lab/CC.h
+++ Documents/lab/CC.h
@@ -2,49 +2,49 @@
 #define SENECA_CC_H
 
 #include <iostream>
-#include "cstr.h"
 
 namespace seneca {
 
-class CC {
-    char* m_name{};
-    unsigned long long m_number{};
-    short m_cvv{};
-    short m_expMonth{};
-    short m_expYear{};
+   class CC {
+      char* m_name;
+      unsigned long long m_number;
+      short m_cvv;
+      short m_expMon;
+      short m_expYear;
 
-    void aloCopy(const char* name);
-    void deallocate();
-    bool validate(const char* name,
-                  unsigned long long number,
-                  short cvv,
-                  short expMon,
-                  short expYear) const;
-    void prnNumber(unsigned long long ccnum) const;
-    void display(const char* name,
-                 unsigned long long number,
-                 short expYear,
-                 short expMon,
-                 short cvv) const;
+      void aloCopy(const char* name);
+      void dealloc();
+      bool validate(const char* name,
+                    unsigned long long cardNo,
+                    short cvv,
+                    short expMon,
+                    short expYear) const;
+      void prnNumber(unsigned long long CCnum) const;
+      void display(const char* name,
+                   unsigned long long number,
+                   short expYear,
+                   short expMon,
+                   short cvv) const;
 
-public:
-    CC();
-    CC(const char* name,
-       unsigned long long number,
-       short cvv,
-       short expMon = 12,
-       short expYear = 26);
-    ~CC();
+   public:
+      CC();
+      CC(const char* name,
+         unsigned long long number,
+         short cvv,
+         short expMon = 12,
+         short expYear = 26);
+      ~CC();
 
-    void set();
-    void set(const char* name,
-             unsigned long long number,
-             short cvv,
-             short expMon,
-             short expYear);
-    bool isEmpty() const;
-    void display() const;
-};
+      void set();
+      void set(const char* name,
+               unsigned long long number,
+               short cvv,
+               short expMon,
+               short expYear);
+
+      bool isEmpty() const;
+      void display() const;
+   };
 
 }
 
