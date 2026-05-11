36306747af41f804ea7b5eac50bf9ce1d7c3747b
raselah15 <raselahmmed1520@gmail.com>
2026-05-11 10:13:48 -0400

    Week 1 Assignment

---
+++ Desktop/helloworld/server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+
+var app = express();
+
+var HTTP_PORT = process.env.PORT || 8080;
+
+// Root Route
+app.get("/", (req, res) => {
+    res.send(`
+        <h1>Hello World!</h1>
+        <br/>
+        <a href="/about">Go to /about route</a>
+    `);
+});
+
+// About Route
+app.get("/about", (req, res) => {
+    res.json({
+        course: "WEB322",
+        student: "RASEL AHMMED",
+        task: "In-class Assignment 1"
+    });
+});
+
+// Start Server
+app.listen(HTTP_PORT, () => {
+    console.log(`Server listening on: http://localhost:${HTTP_PORT}`);
+});
